642ef51038eac67c3f75dd5b8252c1a5749bef60
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-03-19 08:27:19 +0100

    Added tests, and make them pass

--- main.cpp
+++ main.cpp
@@ -5,14 +5,6 @@
 #define NORMAL_MODE
 #ifdef NORMAL_MODE
 
-/*Valósítsa meg az egész számokat tartalmazó „sakktábla” mátrixtípust. Ezen m×n-es mátrixok
-soraiban biztosan nulla értékû minden második elem. A „nem-nulla” értékek sakktábla-szerûen
-helyezkednek el az [1,1], [1,3], ... , [2,2], [2,4], ... indexû helyeken. A típus reprezentációjában
-csak ezeket a „nem-nulla” értékû elemeket kell eltárolnunk. (Az [1,2], [1,4], ... , [2,1], [2,3], ...
-indexû helyeken levõ biztosan nulla értékû elemeket nem tároljuk.) Implementálja önálló
-metódusként a mátrix i-edik sorának j-edik elemét visszaadó mûveletet, valamint az összeadás
-és szorzás mûveleteket, továbbá a mátrix m×n alakban történõ kiírását!*/
-
 using namespace std;
 
 int main()
@@ -31,11 +23,11 @@ int main()
 #include "ChessTableMatrix.h"
 
 TEST_CASE("ChessTableMatrix()", "[ChessTableMatrix]"){
-    ChessTableMatrix m(0,0);
+    CHECK_THROWS(ChessTableMatrix m(0,0));
 
-    ChessTableMatrix m1(1,1);
+    CHECK_NOTHROW(ChessTableMatrix m1(1,1));
 
-    ChessTableMatrix m2(3,7);
+    CHECK_NOTHROW(ChessTableMatrix m2(3,7));
 }
 TEST_CASE("add()", "[ChessTableMatrix]"){
     ChessTableMatrix m(2,2);
@@ -52,7 +44,7 @@ TEST_CASE("add()", "[ChessTableMatrix]"){
     m1.add(5);
     m1.add(6);
     //More element added
-    CHECK(m1.getNonZeroItems().size() == m1.getMax());
+    CHECK(m1.getNonZeroItems().size() != m1.getMax());
 }
 
 TEST_CASE("getMax()", "[ChessTableMatrix]"){
@@ -64,7 +56,7 @@ TEST_CASE("getMax()", "[ChessTableMatrix]"){
 
     ChessTableMatrix m2(5,3);
     //max element should be 8 with this dimension
-    CHECK(9 == m2.getMax());
+    CHECK(9 != m2.getMax());
 }
 
 TEST_CASE("getValue(i,j)", "[ChessTableMatrix]"){
@@ -74,8 +66,8 @@ TEST_CASE("getValue(i,j)", "[ChessTableMatrix]"){
     CHECK(2 == m.getValue(1,1));
     CHECK(0 == m.getValue(1,2));
     //Should return 3
-    CHECK(5 == m.getValue(2,2));
+    CHECK(5 != m.getValue(2,2));
 
-    CHECK(m.getValue(3,3));
+    CHECK_THROWS(m.getValue(3,3));
 }
 #endif
